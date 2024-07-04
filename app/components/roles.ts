b58f4fb12e9756c711636f88cd483a805b0985fd
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-07-05 01:08:34 +0300

    build fix

--- app/components/roles.ts
+++ app/components/roles.ts
@@ -9,6 +9,8 @@ const roles: Roles = {
       abilities: 'Heal one person each night, preventing them from dying.',
       attributes: 'You may only heal yourself once. You will know if your target is attacked.',
       goal: 'Lynch every criminal and evildoer.',
+      limit: 0,
+      currentCount: 0
     },
     // ... other town roles
   ],
@@ -19,6 +21,8 @@ const roles: Roles = {
       abilities: 'Kill someone each night.',
       attributes: "You can't be killed at night. If there is a Mafioso, he will attack the target instead of you. You wake up with the other Mafia at night.",
       goal: 'Kill anyone that will not submit to the Mafia.',
+      limit: 0,
+      currentCount: 0
     },
     // ... other mafia roles
   ],
@@ -29,6 +33,8 @@ const roles: Roles = {
       abilities: 'Trick the Town into voting against you.',
       attributes: 'If you are lynched, you may kill one of your guilty or abstaining voters the following night.',
       goal: 'Get yourself lynched by any means necessary.',
+      limit: 0,
+      currentCount: 0
     },
     // ... other neutral roles
   ],
